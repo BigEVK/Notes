3c88ca2496ff04df12eeb5333f3c69f45391ac16
Eric VanKirk <eric@ericvankirk.com>
2022-08-13 11:35:44 -0500

    Express and MYSQL addition

--- Notes-MySQL.sql
+++ Notes-MySQL.sql
@@ -1,7 +1,14 @@
-show databases
+SHOW DATABASES;
+SHOW TABLES;
+select database(); -- shows current database
+USE <database>; -- Switches to that database
 CREATE TABLE
+SELECT * FROM candidates; -- a query that will return all rows of data in a table, this one is called candidates
+
 VARCHAR - variable character
 
+
+DROP DATABASE election;
 
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -21,3 +28,54 @@ VALUES
 ('Montague', 'Summers', 1),
 ('Octavia', 'Butler', 1),
 ('Unica', 'Zurn', 1);
+
+-- To update rows in a table you name the table to UPDATE, name which filed is to be updated in SET,
+-- and name which specific id is to be updated using WHERE
+UPDATE candidates
+SET industry_connected = 1
+WHERE id = 3;
+
+-- to deleted a row 
+DELETE FROM candidates
+WHERE first_name = "Montague"; -- If the primary key is known, this is preferred over using a name
+
+help -- List of all MySQL commands:
+Note that all text commands must be first on line and end with ';'
+-- ?         (\?) Synonym for `help'.
+clear     (\c) Clear the current input statement.
+connect   (\r) Reconnect to the server. Optional arguments are db and host.
+delimiter (\d) Set statement delimiter.
+ego       (\G) Send command to mysql server, display result vertically.
+exit      (\q) Exit mysql. Same as quit.
+go        (\g) Send command to mysql server.
+help      (\h) Display this help.
+notee     (\t) Don't write into outfile.
+print     (\p) Print current command.
+prompt    (\R) Change your mysql prompt.
+quit      (\q) Quit mysql.
+rehash    (\#) Rebuild completion hash.
+source    (\.) Execute an SQL script file. Takes a file name as an argument.
+status    (\s) Get status information from the server.
+system    (\!) Execute a system shell command.
+tee       (\T) Set outfile [to_outfile]. Append everything into given outfile.
+use       (\u) Use another database. Takes database name as argument.
+charset   (\C) Switch to another charset. Might be needed for processing binlog with multi-byte charsets.
+warnings  (\W) Show warnings after every statement.
+nowarning (\w) Don't show warnings after every statement.
+
+-- If you get an error stating that the database or table does not exist, 
+-- you can start over and create the database again by running the following command:
+source db/db.sql
+-- This will execute the SQL commands in the db.sql file to drop the election database and create it again.
+-- To check that the election database was created
+SHOW DATABASES;
+-- create the table for candidates
+source db/schema.sql
+-- check that the candidates table was created correctly
+SHOW TABLES;
+-- seed the candidates table
+source db/seeds.sql
+-- check whether the database is correctly seeded and ready to go
+SELECT * FROM candidates;
+-- To exit the MySQL command line, type quit; or exit;.
+
--- notes-Express.js
+++ notes-Express.js
@@ -12,6 +12,12 @@ fetch(queryUrl)
   });
 //   -------------END------------------------------------------
 
+//  ---------------- START TEMPLATE ------------------------
+// -------- Possibly a template for many Express.js Connection ---------------
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
 // ********* Every time we create a server that will serve a front and 
 // as well as JSON DataTransfer, and create a server that's looking to 
 // accept POST data we need to use the following code ***********
@@ -21,3 +27,48 @@ app.use(express.json());
 app.use(express.statistis('<directory name>'));
 
 // ************* END middleware functions **********************
+
+// ----- TEST to see if it is running ------------
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World'
+  });
+});
+// ------ END TEST ------------------------
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+//  -------------- END TEMPLATE --------------------------------
+
+//  -------START----------this is from MODULE 11 Challenge ----------------------
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes');
+
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+// This goes in the db.json file
+[
+  {
+    "title": "Test",
+    "text": "Test",
+    "id": "e1356850-0f0c-11eb-86a6-0b297e79cf71"
+  }
+]
+
+//  ----------END-------------Module 11 Challenge notes -------------------
+
